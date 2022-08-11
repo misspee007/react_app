3920bf02335d04107e8a017e643ec5e8f08520c2
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-08-11 16:04:43 +0100

    fix: mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,47 +6,49 @@ import logo from "../assets/images/logo.svg";
 import Button from "./Button";
 
 const Navbar = () => {
-  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+	const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-  const toggleHamburger = () => {
-    hamburgerOpen ? setHamburgerOpen(false) : setHamburgerOpen(true);
-  };
+	const toggleHamburger = () => {
+		hamburgerOpen ? setHamburgerOpen(false) : setHamburgerOpen(true);
+	};
 
-  return (
-    <nav className="nav">
-        <Link to="/shortly" className="logo">
-          <img src={logo} alt="Shortly logo" />
-        </Link>
+	return (
+		<nav className="nav">
+			<Link to="/shortly" className="logo">
+				<img src={logo} alt="Shortly logo" />
+			</Link>
 
-        <ul className="menu">
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">
-              <Button type="btn btn-rounded-sm btn-sm">Sign Up</Button>
-            </Link>
-          </li>
-        </ul>
+			<ul className="menu">
+				<span className="menu-top">
+					<li>Features</li>
+					<li>Pricing</li>
+					<li>Resources</li>
+				</span>
+        <hr />
+				<span className="menu-btm">
+					<li>
+						<Link to="/login">Login</Link>
+					</li>
+					<li>
+						<Link to="/signup">
+							<Button type="btn btn-rounded btn-rg">Sign Up</Button>
+						</Link>
+					</li>
+				</span>
+			</ul>
 
-      <div className="menu-icon" onClick={toggleHamburger}>
-        <i className={hamburgerOpen ? "bi bi-x" : "bi bi-list"}></i>
-      </div>
+			<div className="menu-icon" onClick={toggleHamburger}>
+				<i className={hamburgerOpen ? "bi bi-x" : "bi bi-list"}></i>
+			</div>
 
-      <style jsx="true">{`
+			<style jsx="true">{`
        @media screen and (max-width: 768px){
         .menu{
           display: ${hamburgerOpen ? "inline" : "none"};
         }
       `}</style>
-    </nav>
-  );
+		</nav>
+	);
 };
 
 export default Navbar;
